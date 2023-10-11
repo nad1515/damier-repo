befedeef775d857c607a114b9f93a873923d8a7f
nadia <anabelle0025@yahoo.fr>
2023-10-11 09:04:43 +0200

    script-fini

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 console.log("chargé script");
 
-let arrayLetter = ["A", "B","C","D","E","F","G","H"]
+let arrayLetter = ["A", "B","C","D","E","F","G","H"];
 let monDamier = document.createElement("damier");
 document.body.appendChild(monDamier);
 
@@ -22,37 +22,15 @@ for (let lignes = 0; lignes <= 8; lignes++) {
         ? (maCellule.style.backgroundColor = "black")
         : (maCellule.style.backgroundColor = "white");
         }
+
         
-        if (lignes == 0) {
-            maCellule.style.backgroundColor = "";
+         if (lignes == 0) {
+             maCellule.style.backgroundColor = "";
         };
-         if (lignes == 0 && cellules == 1 ) { 
-            maCellule.textContent = arrayLetter[0] 
-        };
-        if (lignes == 0 && cellules == 2 ) { 
-            maCellule.textContent = arrayLetter[1] 
-        };
-        if (lignes == 0 && cellules == 3 ) { 
-            maCellule.textContent = arrayLetter[2] 
-        };
-        if (lignes == 0 && cellules == 4 ) { 
-            maCellule.textContent = arrayLetter[3] 
-        };
-        if (lignes == 0 && cellules == 5 ) { 
-            maCellule.textContent = arrayLetter[4] 
-        };
-        if (lignes == 0 && cellules == 6 ) { 
-            maCellule.textContent = arrayLetter[5] 
-        };
-        if (lignes == 0 && cellules == 7 ) { 
-            maCellule.textContent = arrayLetter[6] 
-        };
-        if (lignes == 0 && cellules == 8 ) { 
-            maCellule.textContent = arrayLetter[7] 
-        };
-       
-        // if (lignes == 0 && cellules >= 1) { 
-        //     }    maCellule.textContent = arrayLetter[cellules-1]
+        
+         if (lignes == 0 && cellules >= 1) { 
+              maCellule.textContent = arrayLetter[cellules-1] 
+     };
      }
     
 }
